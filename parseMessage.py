2c3df2fe059950af3f62f218a9d740a4e19779ce
r_scop <r.scop@Hotmail.com>
2020-09-19 19:41:35 -0300

    debug

--- parseMessage.py
+++ parseMessage.py
@@ -5,8 +5,52 @@ from getConfigFile import get_ConfigFile
 import database
 import parser_paodeacucar
 import sys
+import base64
 
-env = get_ConfigFile(sys.argv[0]+'.env', 'production')
+# env = get_ConfigFile(sys.argv[0]+'.env', 'production')
+
+def audioToText(data):
+    # url = 'https://speech.googleapis.com/v1/speech:recognize?key=%s'%env['GOOGLE_API_KEY']
+    url = 'https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyAwuBkofALGvA4_bpbP7esiWb1WoyXGo88'
+
+    headers = {'content-type': 'application/json'}
+
+    payload = {
+        "config": {
+            "encoding": "OGG_OPUS",
+            "sampleRateHeartz": 16000,
+            "languageCode": "pt-BR"
+        },
+        "audio": {
+            "content":str(data)
+        }
+    }
+
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+
+def get_as_base64(url):
+
+    # encoded_string = base64.b64encode(requests.get(url).content)
+
+    # r = requests.get(url)
+
+    # with open("tts.ogg", "wb") as o:
+    #     o.write(r.content)
+    
+    # response = open('tts.ogg', "r", encoding='latin1')
+    
+    # msg = response.read()
+    
+    # print(base64.b64encode(b'%s'%encoded_string))
+
+    with sr.AudioFile(filename) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text = r.recognize_google(audio_data)
+        print(text)
+
+get_as_base64('http://chat.zenvia.com/storage/files/9416c34b3d89838f526ff8fe924b11e904044c6ebb6352be749693b55fc8dafd.bin')
 
 def sendMsg(response):
 
@@ -53,8 +97,9 @@ def readMsg(data):
 
     else:
         response = parser_paodeacucar.searchProduct(message)
-        print(response)
+
         sendMsg(str(response))
+
         # database.startChat(name, number)
         # database.insertHistory(name, number)
 
