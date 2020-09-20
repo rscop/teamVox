31a41196602cbae898ec0ad46b24105ec98507e9
r_scop <r.scop@Hotmail.com>
2020-09-20 20:25:54 -0300

    fixing path

--- parseMessage.py
+++ parseMessage.py
@@ -13,8 +13,8 @@ import unidecode
 import os
 
 env = get_ConfigFile(sys.argv[0]+'.env', 'production')
-config = get_ConfigFile(sys.argv[0]+'.cfg', 'production')
-# env = get_ConfigFile('webservice.py'+'.env', 'production')
+# config = get_ConfigFile(sys.argv[0]+'.cfg', 'production')
+env = get_ConfigFile('webservice.py'+'.env', 'production')
 
 # SetupLOG Funnction
 def setuplog(lf,lfBkpCnt,logLevel):
